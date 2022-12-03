6b2bed672785c656826403f21701611021a29e14
Ayub <mrkou65@gmail.com>
2022-12-03 12:07:04 +0330

    Download from url

--- unique-word-extractor.py
+++ unique-word-extractor.py
@@ -1,22 +1,25 @@
 import re
 from os.path import exists
 from hazm import *
+import requests
 
 if not exists("unique-words.txt"):
     open("unique-words.txt", 'a').close()
 
-# Cleaning
-text = open('input.txt', 'r', encoding='utf-8').read()
-text = Normalizer().normalize(text)
-text = re.sub(r'[^آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی ]', ' ', text)
-words = list(set(WordTokenizer().tokenize(text)))
+urls = open('urls.txt', "r", encoding="utf-8").read().splitlines()
 
-existing_words = open('unique-words.txt', "r",
-                      encoding="utf-8").read().splitlines()
+for url in urls:
+    text = requests.get(url).text
 
-existing_words = list(filter(None, existing_words))
-combine_list = list(set(words + existing_words))
+    text = Normalizer().normalize(text)
+    text = re.sub(r'[^آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی ]', ' ', text)
+    words = list(set(WordTokenizer().tokenize(text)))
 
+    existing_words = open('unique-words.txt', "r",
+                          encoding="utf-8").read().splitlines()
 
-with open('unique-words.txt', 'w', encoding='utf-8') as f:
-    f.write('\n'.join(combine_list))
+    existing_words = list(filter(None, existing_words))
+    combine_list = list(set(words + existing_words))
+
+    with open('unique-words.txt', 'w', encoding='utf-8') as f:
+        f.write('\n'.join(combine_list))
